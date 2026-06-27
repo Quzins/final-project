7b8862fa79c8659af0e113a8af98e92eed5299ad
Nurdolot2101 <brownguyab422@gmail.com>
2026-06-27 16:30:44 +0600

    pages

---
+++ Final/src/pages/home.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const  [count, setCount] = useState(0)
+
+  return (
+    <>
+    <section className='Headline'>
+
+    </section>
+    <section className='info'> <h4> это современная независимая медиа-платформа,
+        созданная болельщиками для болельщиков.Мы объединяем любителей футбола со всего мира,
+         предоставляя честный взгляд на игру без цензуры официальных клубов и корпоративных СМИ.
+         </h4>
+         </section>
+         <section className='pages'>
+           <a href="http://"></a>
+         </section>
+    </>
+  )
+}
+
+export default App
