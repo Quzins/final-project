c5326174b6eddf73351b041aa4780df679fd0402
Daniar0707 <top4ikaligiga@gmail.com>
2026-06-28 15:34:29 +0600

    glava

--- Final/src/App.jsx
+++ Final/src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
+import './home.css'
 
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const  [count, setCount] = useState(0)
 
   return (
     <>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+    <section className='Headline'>
+
+    </section>
+    <section className='info'> <h4> это современная независимая медиа-платформа,
+        созданная болельщиками для болельщиков.Мы объединяем любителей футбола со всего мира,
+         предоставляя честный взгляд на игру без цензуры официальных клубов и корпоративных СМИ.
+    </h4>
+    </section>
+    <section className='pages'>
+           <a href="http://">Комада</a>
+           <a href="http://">Игроки</a>
+           <a href="http://">Ставки на спорт</a>
+           <a href="http://">Матчи</a>
+    </section>
     </>
+
   )
 }
 
