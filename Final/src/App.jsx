e070c079e465afa271d7dbdb6b854673765ea0f7
Nurdolot2101 <brownguyab422@gmail.com>
2026-07-05 14:37:16 +0600

    pages

--- Final/src/App.jsx
+++ Final/src/App.jsx
@@ -8,23 +8,23 @@ import './App.css'
 function App() {
   const  [count, setCount] = useState(0)
 
- return (
-     <>
-     <section className='Headline'>
+//  return (
+//      <>
+//      <section className='Headline'>
     
-    </section>
-    <section className='info'> <h4> это современная независимая медиа-платформа,
-        созданная болельщиками для болельщиков.Мы объединяем любителей футбола со всего мира,
-         предоставляя честный взгляд на игру без цензуры официальных клубов и корпоративных СМИ.
-    </h4>
-    </section>
+//     </section>
+//     <section className='info'> <h4> это современная независимая медиа-платформа,
+//         созданная болельщиками для болельщиков.Мы объединяем любителей футбола со всего мира,
+//          предоставляя честный взгляд на игру без цензуры официальных клубов и корпоративных СМИ.
+//     </h4>
+//     </section>
 
-    <section className='pages'>
-           <a href="http://">Комада</a>
-           <a href="http://">Игроки</a>
-           <a href="http://">Ставки на Футболл</a>
-           <a href="http://">Матчи</a>
-    </section> */
+//     <section className='pages'>
+//            <a href="http://">Комада</a>
+//            <a href="http://">Игроки</a>
+//            <a href="http://">Ставки на Футболл</a>
+//            <a href="http://">Матчи</a>
+//     </section> */
 
 
  
@@ -39,7 +39,9 @@ function App() {
     </>
 
   )
+  
 
 }
+
 
 export default App
