01446d8e458646852c3bd8d83e1295b314bbc771
Nurdolot2101 <brownguyab422@gmail.com>
2026-07-05 14:56:52 +0600

    pages

--- Final/src/App.jsx
+++ Final/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route, L } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import axios from 'axios'
 import Teams from './team/Teams'
 import './App.css'
@@ -8,29 +8,28 @@ import './App.css'
 function App() {
   const  [count, setCount] = useState(0)
 
-//  return (
-//      <>
-//      <section className='Headline'>
-    
-//     </section>
-//     <section className='info'> <h4> это современная независимая медиа-платформа,
-//         созданная болельщиками для болельщиков.Мы объединяем любителей футбола со всего мира,
-//          предоставляя честный взгляд на игру без цензуры официальных клубов и корпоративных СМИ.
-//     </h4>
-//     </section>
 
-//     <section className='pages'>
-//            <a href="http://">Комада</a>
-//            <a href="http://">Игроки</a>
-//            <a href="http://">Ставки на Футболл</a>
-//            <a href="http://">Матчи</a>
-//     </section> */
 
 
  
 
   return (
     <>
+    <section className='Headline'>
+    
+     </section>
+     <section className='info'> <h4> это современная независимая медиа-платформа,
+         созданная болельщиками для болельщиков.Мы объединяем любителей футбола со всего мира,
+          предоставляя честный взгляд на игру без цензуры официальных клубов и корпоративных СМИ.
+     </h4>
+     </section>
+
+     <section className='pages'>
+            <a href="http://">Комада</a>
+            <a href="http://">Игроки</a>            
+            <a href="http://">Ставки на Футболл</a>
+            <a href="http://">Матчи</a>
+     </section> */
       <BrowserRouter>
       <Routes>
       <Route path="/teams" element={<Teams />}/>
--- Final/src/pages/home.jsx
+++ Final/src/pages/home.jsx
@@ -1,31 +1,46 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import axios from 'axios'
+import Teams from './team/Teams'
 import './App.css'
 
 
 function App() {
   const  [count, setCount] = useState(0)
 
+
+
+
+ 
+
   return (
     <>
     <section className='Headline'>
-        <h1>Goal-Data</h1>
-    </section>
+    
+     </section>
+     <section className='info'> <h4> это современная независимая медиа-платформа,
+         созданная болельщиками для болельщиков.Мы объединяем любителей футбола со всего мира,
+          предоставляя честный взгляд на игру без цензуры официальных клубов и корпоративных СМИ.
+     </h4>
+     </section>
 
-    <section className='info'> <h4> это современная независимая медиа-платформа,
-        созданная болельщиками для болельщиков.Мы объединяем любителей футбола со всего мира,
-         предоставляя честный взгляд на игру без цензуры официальных клубов и корпоративных СМИ.
-    </h4>
-    </section>
-    <section className='pages'>
-           <Link to="/Teams"></Link>
-           <Link to=""></Link>
-           <Link to=""></Link>
-           <Link to=""></Link>
-    </section>
+     <section className='pages'>
+            <a href="http://">Комада</a>
+            <a href="http://">Игроки</a>            
+            <a href="http://">Ставки на Футболл</a>
+            <a href="http://">Матчи</a>
+     </section> */
+      <BrowserRouter>
+      <Routes>
+      <Route path="/teams" element={<Teams />}/>
+      </Routes>
+      </BrowserRouter>
     </>
 
   )
+  
+
 }
+
 
 export default App
