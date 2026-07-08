a7b79fd45f74516d5a7e7847fcc7e799dbdaa476
Daniar0707 <top4ikaligiga@gmail.com>
2026-07-08 14:08:33 +0600

    stili

--- src/players/players.jsx
+++ src/players/players.jsx
@@ -4,13 +4,24 @@ import "./players.css";
 
 function Players() {
   const [selectedTeam, setSelectedTeam] = useState(null);
+  const positions = [
+
+{ title: "Вратари", value: "Goalkeeper" },
+
+{ title: "Защитники", value: "Defender" },
+
+{ title: "Полузащитники", value: "Midfielder" },
+
+{ title: "Нападающие", value: "Forward" },
+
+];
 
   return (
     <div>
 
       <h1>Выбери команду</h1>
 
-      <div>
+      <div className="teams">
         {players.map((team, index) => (
           <button
             key={index}
@@ -22,20 +33,27 @@ function Players() {
       </div>
 
 
-      {selectedTeam && (
-        <div>
-          <h2><span><img src="" alt="" /></span>{selectedTeam.team}</h2>
+{selectedTeam && (
+  <div>
+    <h2>{selectedTeam.team}</h2>
 
-          <h3>Игроки:</h3>
+    {positions.map(pos => (
+      <div key={pos.value}>
+        <h2>{pos.title}</h2>
 
-          {selectedTeam.players.map((player) => (
+        {selectedTeam.players
+          .filter(player => player.position === pos.value)
+          .map(player => (
             <div key={player.id} className="player-card">
               <h4>{player.name}</h4>
-              <p>{player.position}</p>
             </div>
           ))}
-        </div>
-      )}
+
+      </div>
+    ))}
+
+  </div>
+)}
 
     </div>
   );
